6a75978a7c123f2bc314fea05ee1d931bc5626d9
HiradJF <hiradjahangirfard@gmail.com>
2025-09-08 22:18:08 +0330

    First Public Release V1.0.0 : Published, added .gitignore, added __init__.py, added LICENSE and added pyproject.toml and fixed the file structure . This package is now ready to use.

--- src/mktable/mktable.py
+++ src/mktable/mktable.py
@@ -4,43 +4,65 @@
 Created on Mon Aug 25 13:33:24 2025
 
 @author: Hirad Jahangirfard (@HiradJF)
+
+mktable:
+
+a simple, efficient, super customizable table class
+that's easy to use and does most of things you need
+in showing a working yet sleek CLI table.
 """
 
-class Table():
+
+class Table:
     """
-    mktable.Table is a class of a table as expected
+    mktable.Table is the class of mktable table as expected
     Make an instance by : table = mktable.Table(["Row","1"])
     Made by : Hirad jahangirfard
-    Documentaition : ./README.MD 
+    Documentaition : ./README.MD
     """
-    def __init__(self, header:list, *rows:list):
-        '''The __init__ constructor of the table'''
-        self.rows = [row for row in rows] #adds rows to self.rows
+
+    def __init__(self, header: list, *rows: list):
+        """
+        The __init__ constructor of the table
+        """
+        self.rows = [row for row in rows]  # adds rows to self.rows
         self.rows.insert(0, header)
-        self.h_sep_char = '-'
-        self.v_sep_char = '|'
-        self.connection_point = '+'
-        self.empty = ''
-    def add_row(self, *new_rows:list):
-        
+        self.h_sep_char = "-"
+        self.v_sep_char = "|"
+        self.connection_point = "+"
+        self.empty = ""
+
+    def add_row(self, *new_rows: list):
+        """
+        adds given rows from the *new_rows arguement
+        """
         for nr in new_rows:
             self.rows.append(nr)
 
-    def get_columns (self) -> list:
+    def get_columns(self) -> list:
+        """
+        Transforms the rows and returnes the column
+        """
         columns = []
         rows = self.fill_rows_gaps()
         for i in range(len(rows[0])):
             column = [row[i] for row in rows]
             columns.append(column)
         return columns
-    
-    def remove_row(self, *index):
+
+    def remove_row(self, *index, **kwargs):
+        """
+        Removes the row with the given index(s) : *index
+        Or by the content with *kwargs
+        Use like rm1 = ["Hi", "this row", "must be deleted"]
+        """
         for i in range(len(self.rows)):
             self.rows.pop(index[i])
-    
+
     def move_row(self, old_index, new_index):
+        """Moves the row in old_index to new_index"""
         self.rows.insert(new_index, self.rows.pop(old_index))
-    
+
     def render(self) -> list:
         """
         Renders out the table and returns it in a list.
@@ -52,64 +74,69 @@ class Table():
         h_sep_char = self.h_sep_char
         v_sep_char = self.v_sep_char
         connection_point = self.connection_point
-        
+
         column_widths = self.get_columns_widths()
-        max_column_widths =  [max(col_width) for col_width in column_widths]
-        
+        max_column_widths = [max(col_width) for col_width in column_widths]
 
-
-        def format_row(row:list) -> list:
+        def format_row(row: list) -> list:
             """formats the row and returns it so that it's spacing and seperators are fixed"""
             formatted_row = []
-            for i in range(len(row)):
-                #getting the diffrence between the needed width and the actual width
-                diff = max_column_widths[i] - len(row[i])
-                #formatting the cell {0} is the vertical sep {1} is the unformatted cell from rows
-                #{2} is the needed spacing
+            for i, row in enumerate(row):
+                # getting the diffrence between the needed width and the actual width
+                diff = max_column_widths[i] - len(row)
+                # formatting the cell {0} is the vertical sep {1} is the unformatted cell from rows
+                # {2} is the needed spacing
                 # so it should look like this |cell   | the space replicates spacing
-                formatted_cell = f"{v_sep_char} {row[i]}{' '*diff} "              
+                formatted_cell = f"{v_sep_char} {row}{' '*diff} "
                 formatted_row.append(formatted_cell)
-            
+
             return ("".join(formatted_row)) + v_sep_char
-        
-        
+
         f_rows = list(map(format_row, rows))
         border_len = len(f_rows[0]) - 2
-        horizontal_separator = connection_point + (h_sep_char * border_len) + connection_point
+        horizontal_separator = (
+            connection_point + (h_sep_char * border_len) + connection_point
+        )
 
-        
         f_rows.insert(0, horizontal_separator)
         f_rows.insert(2, horizontal_separator)
         f_rows.append(horizontal_separator)
-        #finaly returns the rendered in list ! of string !
+        # finaly returns the rendered in list ! of string !
         return f_rows
-        
-        
+
     def __str__(self):
         rows = self.render()
         n_rows = list(map(lambda row: row + "\n", rows))
-        return "".join(n_rows)        
-    
+        return "".join(n_rows)
+
     def get_columns_widths(self) -> list:
-        #for getting the printable(max) width of each column :
+        """
+        You can get every columns width in a list with this method.
+        """
+        # for getting the printable(max) width of each column :
         max_column_widths = []
         columns = self.get_columns()
         for column in columns:
             column_widths = []
             for cell in column:
-                #gets the column widths (width of each row in column)
+                # gets the column widths (width of each row in column)
                 column_widths.append(len(cell))
-            #gets the max width for this column
+            # gets the max width for this column
             max_column_widths.append(column_widths)
         # and this loop ^ gets all the columns max widths
-        #why? we need max widths for spacing and the table being inline
+        # why? we need max widths for spacing and the table being inline
         return max_column_widths
-    
+
     def get_rows_lengths(self):
+        """You can get every row's length with this method"""
         rows = self.rows
         return [len(row) for row in rows]
-    
+
     def fill_rows_gaps(self) -> list:
+        """
+        This method gets an copy of rows, fills it's empty gaps with self.empty and returns it.
+        This makes it efficient to print and is used internally.
+        """
         rows = self.rows
         rows_lengths = self.get_rows_lengths()
         max_rows_len = max(rows_lengths)
@@ -117,7 +144,9 @@ class Table():
             diff = max_rows_len - rows_lengths[i]
             rows[i] += [self.empty] * diff
         return rows
-    
-t = Table(["First Name","Last Name"], ["Hirad", "Jahangirfard"])
-t.add_row(["Behrad", "Jahangirfard"], ["Taher", "Jahangirfard"], ["Masoomeh", "Fooladi"], ["Someone"])
-print(t)
+
+if __name__ == '__main__':
+    t = Table(['First Name', 'Last Name', 'Age', 'Email'])
+    t.add_row(['Hirad', 'Jahangirfard', '13', 'hiradjahangirfard@jmail.com'], ['Behrad', 'Jahangirfard'])
+    t.add_row(['Taher', 'Jahangirfard', '48', 'jahangiri_it@yahoo.com'])
+    print(t)
